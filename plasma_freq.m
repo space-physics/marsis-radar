d7744ff2a5c95e8add96172500edae0d747ddfeb
scienceopen <scienceopen@users.noreply.github.com>
2017-01-01 00:03:14 -0500

    correct param'

--- plasma_freq.m
+++ plasma_freq.m
@@ -2,9 +2,9 @@ eps0 = 8.854187817e-12; % F/m
 me = 9.10938356e-31;  %kg
 q = 1.60217662e-19; % coulombs
 
-fpe = 1e6; % Hz
+fpe = .109e6; % Hz
 omegape = 2*pi*fpe;
 
 ne = omegape^2 * eps0 * me / q^2
 
-const = 1/(2*pi) * sqrt(q^2 / (eps0*me))
+const = 1/(2*pi)^2 * eps0*me/q^2
